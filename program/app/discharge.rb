919e269945ec61e6d861bcc763a7fa89f81172ed
righ1113 <pykp42557@mineo.jp>
2020-11-02 05:34:51 +0900

    20/11/02 Discharge : メインルーチン

--- program/app/discharge.rb
+++ program/app/discharge.rb
@@ -15,13 +15,24 @@ class Discharge
   include ReadFile
   include LibReduce
 
-  def self.discharge(deg = 7)
-    puts "中心の次数 deg: #{deg}"
+  MAXVAL     = 12
+  CARTVERT   = 5 * MAXVAL + 2 # domain of l_A, u_A, where A is an axle
+  MAXLEV     = 12             # max level of an input line + 1
+
+  def self.discharge(degree = 7)
+    # deg と axles
+    @deg = degree
+    puts "中心の次数 deg: #{@deg}"
+    @axles = {
+      low: Array.new(MAXLEV + 1, Array.new(CARTVERT, 0)),
+      upp: Array.new(MAXLEV + 1, Array.new(CARTVERT, 0)),
+      lev: 0
+    }
 
     # LibReduce クラスのインスタンスを作る
-    reduce = LibReduce.new
-    reduce.r_axles[:low][0][3] = 7
-    p reduce.r_axles[:low][0]
+    i_reduce = LibReduce.new
+    i_reduce.r_axles[:low][0][3] = 7
+    p i_reduce.r_axles[:low][0]
 
     # Rules クラスのインスタンスを作る
     rules = Rules.new
@@ -35,14 +46,23 @@ class Discharge
     p tactics.tacs[2]
     p tactics.tacs[13]
 
-    # main loop
-    tactics.tacs.each_with_index do |tac, i|
-      next if i.zero? # 下に空行を入れるらしい
+    ret = main_loop i_reduce, rules, tactics
+    # final check
+    if ret == 'Q.E.D.'
+      puts "中心の次数 #{@deg} のグラフは、電荷が負になるか、近くに好配置があらわれるかです。"
+    else
+      puts '実装中です。'
+    end
+  end
 
-      break if tac[0] == 'Q.E.D' # 暫定脱出
+  def self.main_loop(_i_reduce, _rules, tactics)
+    tactics.tacs.each_with_index do |tac, i|
+      break 'Q.E.D.' if tac[0] == 'Q.E.D.' # 下に空行を入れるらしい
 
       puts "#{i}: #{tac}"
       case tac[1]
+      when '7', '8', '9', '10', '11'
+        puts '0th line.'
       when 'S'
         puts 'Symmetry.'
       when 'R'
@@ -54,7 +74,7 @@ class Discharge
       else
         raise "無効なtactic: #{tac}"
       end
-      break if tac[1] == 'S' # 暫定脱出
+      break 'ahaha' if tac[1] == 'S' # 暫定脱出
     end
   end
 end
