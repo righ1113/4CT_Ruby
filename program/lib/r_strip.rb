4d4b93ea5d6c33d56b214ea2196b127cf1971f81
righ1113 <pykp42557@mineo.jp>
2021-02-05 00:05:01 +0900

    21/02/05 reduce : Strip の実装3

--- program/lib/r_strip.rb
+++ program/lib/r_strip.rb
@@ -22,13 +22,81 @@ module Strip
         @edgeno[u][v] = v
         @edgeno[v][u] = v
       end
-      _done0 = Array.new(Const::MVERTS, false)
-      _term  = 3 * (verts - 1) - ring
+      done0 = Array.new(Const::MVERTS, false)
+      term  = 3 * (verts - 1) - ring
 
       # 2. stripSub2
       # This eventually lists all the internal edges of the configuration
+      term = strip_sub2 g_conf, verts, ring, done0, term
+
       # 3. stripSub3
       # Now we must list the edges between the interior and the ring
     end
+
+    private
+
+    def strip_sub2(g_conf, verts, ring, done, term)
+      best = 1
+      max  = Array.new(Const::MVERTS, 0)
+
+      ((ring + 1)..verts).each do
+        # First we find all vertices from the interior that meet the "done"
+        # vertices in an interval, and write them in max[1] .. max[maxes]
+        maxint = 0
+        maxes  = 0
+
+        ((ring + 1)..verts).each do |v|
+          next if done[v]
+          inter = in_interval g_conf[v + 2], done
+          if inter > maxint
+            maxint = inter
+            maxes  = 1
+            max[1] = v
+          elsif inter == maxint
+            maxes += 1
+            max[maxes] = v
+          end
+        end
+
+        # From the terms in max we choose the one of maximum degree
+        maxdeg = 0
+        (1..maxes).each do |h|
+          d = g_conf[max[h] + 2][0 + 1]
+          if d > maxdeg
+            maxdeg = d
+            best   = max[h]
+          end
+        end
+        # So now, the vertex "best" will be the next vertex to be done
+
+        d        = g_conf[best + 2][0 + 1]
+        first    = 1
+        previous = done[g_conf[best + 2][d + 1]]
+
+        while previous || !done[g_conf[best + 2][first + 1]]
+          previous = done[g_conf[best + 2][1 + first]]
+          first += 1
+          (first = 1; break) if first > d
+        end
+
+        h = first
+        while done[g_conf[best + 2][h + 1]]
+          @edgeno[best][g_conf[best + 2][h + 1]] = term
+          @edgeno[g_conf[best + 2][h + 1]][best] = term
+          term -= 1
+          if h == d
+            break if first == 1
+            h = 0
+          end
+          h += 1
+        end
+        done[best] = true
+      end
+      # This eventually lists all the internal edges of the configuration
+
+      term
+    end
+
+    def in_interval(_grav, _done) = 0
   end
 end
