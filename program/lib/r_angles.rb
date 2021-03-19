f6ff8ec9021eb07ade8e9dce5f612f118dc5fae8
righ1113 <pykp42557@mineo.jp>
2021-03-20 06:57:26 +0900

    21/03/20 reduce : Angles の実装3

--- program/lib/r_angles.rb
+++ program/lib/r_angles.rb
@@ -8,37 +8,37 @@ module Angles
   class Angles
     include Const
 
-    attr_reader :angle, :diffangle, :sameangle
+    attr_reader :angle, :diffangle, :sameangle, :contract
 
     def initialize(g_conf, edgeno)
-      _edge = 3 * g_conf[0 + 1][0] - 3 - g_conf[0 + 1][1]
+      edge = 3 * g_conf[0 + 1][0] - 3 - g_conf[0 + 1][1]
 
       @angle = Array.new(Const::EDGES) { Array.new(5, 0) }
       @diffangle = Array.new(Const::EDGES) { Array.new(5, 0) }
       @sameangle = Array.new(Const::EDGES) { Array.new(5, 0) }
 
-      contract = Array.new((Const::EDGES + 1), 0)
-      contract[0] = g_conf[1 + 1][0] # number of edges in contract
-      contract[Const::EDGES] = g_conf[0 + 1][3]
-      contract[0].times do |ii|
+      @contract = Array.new((Const::EDGES + 1), 0)
+      @contract[0] = g_conf[1 + 1][0] # number of edges in contract
+      @contract[Const::EDGES] = g_conf[0 + 1][3]
+      @contract[0].times do |ii|
         i = ii + 1
         u = g_conf[2][2 * i - 1]
         v = g_conf[2][2 * i]
-        # Debug.Assert((edgeno.[u].[v] >= 1),
-        #   "         ***  ERROR: CONTRACT CONTAINS NON-EDGE  ***\n\n")
-        contract[edgeno[u][v]] = 1
+        Assert.assert_equal (edgeno[u][v] >= 1), true, '***  ERROR: CONTRACT CONTAINS NON-EDGE  ***'
+        @contract[edgeno[u][v]] = 1
       end
 
-      # for i in 0..(EDGES-1) do
-      #   angle[i][0] = 0
-      #   diffangle[i][0] = 0
-      #   sameangle[i][0] = 0
-      # diffangle.[0].[0] <- g_conf[0+1].[0]
-      # diffangle.[0].[1] <- g_conf[0+1].[1]
-      # diffangle.[0].[2] <- edge
-      # angle.[0].[0]     <- diffangle.[0].[0]
-      # angle.[0].[1]     <- diffangle.[0].[1]
-      # angle.[0].[2]     <- diffangle.[0].[2]
+      (Const::EDGES - 1).times do |i|
+        @angle[i][0] = 0
+        @diffangle[i][0] = 0
+        @sameangle[i][0] = 0
+      end
+      @diffangle[0][0] = g_conf[0 + 1][0]
+      @diffangle[0][1] = g_conf[0 + 1][1]
+      @diffangle[0][2] = edge
+      @angle[0][0] = @diffangle[0][0]
+      @angle[0][1] = @diffangle[0][1]
+      @angle[0][2] = @diffangle[0][2]
 
       # findanglesSub2
       # LibReduceAngle.FindanglesSub2 (graph, edgeno, contract, angle, diffangle, sameangle)
