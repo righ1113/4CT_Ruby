70383d1a811b701e5f35ca935c175af291c3a8d4
righ1113 <pykp42557@mineo.jp>
2021-05-17 18:49:05 +0900

    21/05/17 reduce : findlive() の実装2

--- program/lib/r_findlive.rb
+++ program/lib/r_findlive.rb
@@ -23,7 +23,23 @@ module Findlive
 
     private
 
-    def findlive(_ring, _bigno, _ncodes, _angle, _extentclaim)
+    def findlive(_ring, _bigno, _ncodes, angle, _extentclaim)
+      # computes {\cal C}_0 and stores it in live. That is, computes codes of
+      # colorings of the ring that are not restrictions of tri-colorings of the
+      # free extension. Returns the number of such codes
+
+      ed           = angle[0][2]
+      c            = Array.new(Const::EDGES, 0)
+      j            = ed - 1
+      c[ed]        = 1
+      c[j]         = 2
+      forbidden    = Array.new(Const::EDGES, 0)
+      forbidden[j] = 5
+      # let structureTuple = LibReduceLive.FindliveSub (bigno, angle, POWER, ring, ed, extentclaim, ncodes, live0, j, c, forbidden)
+      #   // 構造体タプルのパターンマッチ
+      # match structureTuple with
+      #   | struct (ncodes1, live1) -> (ncodes1, live1)
+
       @n_live = 7
     end
   end
