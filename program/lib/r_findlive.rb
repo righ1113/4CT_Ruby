5a07b193a6399b6f3a08bddbdde84f7b75451ae5
righ1113 <pykp42557@mineo.jp>
2021-05-24 08:31:37 +0900

    21/05/24 reduce : findlive() の実装3

--- program/lib/r_findlive.rb
+++ program/lib/r_findlive.rb
@@ -23,7 +23,7 @@ module Findlive
 
     private
 
-    def findlive(_ring, _bigno, _ncodes, angle, _extentclaim)
+    def findlive(ring, bigno, ncodes, angle, extentclaim)
       # computes {\cal C}_0 and stores it in live. That is, computes codes of
       # colorings of the ring that are not restrictions of tri-colorings of the
       # free extension. Returns the number of such codes
@@ -35,12 +35,54 @@ module Findlive
       c[j]         = 2
       forbidden    = Array.new(Const::EDGES, 0)
       forbidden[j] = 5
-      # let structureTuple = LibReduceLive.FindliveSub (bigno, angle, POWER, ring, ed, extentclaim, ncodes, live0, j, c, forbidden)
+      findlive_sub bigno, angle, ring, ed, extentclaim, ncodes, j, c, forbidden
       #   // 構造体タプルのパターンマッチ
       # match structureTuple with
-      #   | struct (ncodes1, live1) -> (ncodes1, live1)
+      #   | struct (ncodes1, live1) -> (@n_live, @live)
 
       @n_live = 7
+    end
+
+    def findlive_sub(bigno, angle, ring, edd, extentclaim, ncodes, jjj, ccc, forbidden)
+      # int x, extent=0, u, i;
+      # for (x = 0; x < 262144; x++) {
+      #   while ((forbidden[j] & c[j]) != 0) {
+      #     c[j] <<= 1;
+      #     while ((c[j] & 8) != 0) {
+      #       if (j >= ed - 1) {
+      #           Printstatus(ring, ncodes, extent, extentclaim);
+      #           return ((ncodes - extent), live);
+      #       }
+      #       c[++j] <<= 1;
+      #     }
+      #   }
+      #   if (j == ring + 1) {
+      #     extent = Record(c, power, ring, angle, live, extent, bigno);
+      #     c[j] <<= 1;
+      #     while ((c[j] & 8) != 0) {
+      #       if (j >= ed - 1) {
+      #           Printstatus(ring, ncodes, extent, extentclaim);
+      #           return ((ncodes - extent), live);
+      #       }
+      #       c[++j] <<= 1;
+      #     }
+      #   }
+      #   else {
+      #     --j;
+      #     if (j < 0) {
+      #         return ((ncodes - extent), live);
+      #     }
+      #     c[j] = 1;
+      #     for (u = 0, i = 1; i <= angle[j][0]; i++) {
+      #       if (i >= 5) break;
+      #       u |= c[angle[j][i]];
+      #     }
+      #     forbidden[j] = u;
+      #   }
+      # }
+      # Debug.Assert(false,
+      #   "FindliveSub : It was not good though it was repeated 262144 times!");
+      # return (-1, live);
     end
   end
 end
