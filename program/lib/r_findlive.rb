673eafc3ebc2b151a70360ab5b385b86f009ba11
righ1113 <pykp42557@mineo.jp>
2021-05-26 03:50:40 +0900

    21/05/26 reduce : findlive() の実装4

--- program/lib/r_findlive.rb
+++ program/lib/r_findlive.rb
@@ -28,61 +28,54 @@ module Findlive
       # colorings of the ring that are not restrictions of tri-colorings of the
       # free extension. Returns the number of such codes
 
-      ed           = angle[0][2]
-      c            = Array.new(Const::EDGES, 0)
-      j            = ed - 1
-      c[ed]        = 1
-      c[j]         = 2
-      forbidden    = Array.new(Const::EDGES, 0)
-      forbidden[j] = 5
-      findlive_sub bigno, angle, ring, ed, extentclaim, ncodes, j, c, forbidden
-      #   // 構造体タプルのパターンマッチ
-      # match structureTuple with
-      #   | struct (ncodes1, live1) -> (@n_live, @live)
-
-      @n_live = 7
+      ed             = angle[0][2]
+      c              = Array.new(Const::EDGES, 0)
+      j              = ed - 1
+      c[ed]          = 1
+      c[j]           = 2
+      forbidden      = Array.new(Const::EDGES, 0)
+      forbidden[j]   = 5
+      @n_live, @live = findlive_sub bigno, angle, ring, ed, extentclaim, ncodes, j, c, forbidden
     end
 
     def findlive_sub(bigno, angle, ring, edd, extentclaim, ncodes, jjj, ccc, forbidden)
-      # int x, extent=0, u, i;
-      # for (x = 0; x < 262144; x++) {
-      #   while ((forbidden[j] & c[j]) != 0) {
-      #     c[j] <<= 1;
-      #     while ((c[j] & 8) != 0) {
-      #       if (j >= ed - 1) {
-      #           Printstatus(ring, ncodes, extent, extentclaim);
-      #           return ((ncodes - extent), live);
-      #       }
-      #       c[++j] <<= 1;
-      #     }
-      #   }
-      #   if (j == ring + 1) {
-      #     extent = Record(c, power, ring, angle, live, extent, bigno);
-      #     c[j] <<= 1;
-      #     while ((c[j] & 8) != 0) {
-      #       if (j >= ed - 1) {
-      #           Printstatus(ring, ncodes, extent, extentclaim);
-      #           return ((ncodes - extent), live);
-      #       }
-      #       c[++j] <<= 1;
-      #     }
-      #   }
-      #   else {
-      #     --j;
-      #     if (j < 0) {
-      #         return ((ncodes - extent), live);
-      #     }
-      #     c[j] = 1;
-      #     for (u = 0, i = 1; i <= angle[j][0]; i++) {
-      #       if (i >= 5) break;
-      #       u |= c[angle[j][i]];
-      #     }
-      #     forbidden[j] = u;
-      #   }
-      # }
-      # Debug.Assert(false,
-      #   "FindliveSub : It was not good though it was repeated 262144 times!");
-      # return (-1, live);
+      262_144.times do
+        while (forbidden[jjj] & ccc[jjj]) != 0
+          ccc[jjj] <<= 1
+          while (ccc[jjj] & 8) != 0
+            if j >= ed - 1
+              # Printstatus(ring, ncodes, extent, extentclaim);
+              return [ncodes - extent, @live]
+            end
+            ccc[jjj] <<= 1
+          end
+        end
+        #   if (j == ring + 1) {
+        #     extent = Record(c, power, ring, angle, live, extent, bigno);
+        #     c[j] <<= 1;
+        #     while ((c[j] & 8) != 0) {
+        #       if (j >= ed - 1) {
+        #           Printstatus(ring, ncodes, extent, extentclaim);
+        #           return ((ncodes - extent), live);
+        #       }
+        #       c[++j] <<= 1;
+        #     }
+        #   }
+        #   else {
+        #     --j;
+        #     if (j < 0) {
+        #         return ((ncodes - extent), live);
+        #     }
+        #     c[j] = 1;
+        #     for (u = 0, i = 1; i <= angle[j][0]; i++) {
+        #       if (i >= 5) break;
+        #       u |= c[angle[j][i]];
+        #     }
+        #     forbidden[j] = u;
+        #   }
+      end
+      # Assert.assert_equal (1 == 2), true, 'findlive_sub : It was not good though it was repeated 262144 times!'
+      [-1, @live]
     end
   end
 end
