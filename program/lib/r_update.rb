466e91ef3fa3e774d400c140e6b00361652687c5
righ1113 <34955220+righ1113@users.noreply.github.com>
2021-12-19 08:17:02 +0900

    21/12/19 reduce : Update の実装2

--- program/lib/r_update.rb
+++ program/lib/r_update.rb
@@ -17,15 +17,30 @@ module Update
       nchar   = Const::SIMATCHNUMBER[ring] / 8 + 1
 
       # computes {\cal M}_{i+1} from {\cal M}_i, updates the bits of "real"
-      update ring, nchar, ncodes, n_live, live
+      @n_live2, @live2 = update ring, nchar, ncodes, n_live, live
       # computes {\cal C}_{i+1} from {\cal C}_i, updates "live"
     end
 
     private
 
-    def update(_ring, _nchar, _ncodes, _n_live, _live)
-      # let (nlive2, live2) =
-      p 7
+    def update(ring, nchar, ncodes, n_live, live)
+      i = 0
+      n_live_box = [n_live]
+      # nlive1、liveに破壊的代入をおこなう
+      while i.zero? || (update_sub ncodes, n_live_box, live)
+        # stillreal()でliveに破壊的代入をおこなう
+        testmatch ring, nchar, live
+        # computes {\cal M}_{i+1} from {\cal M}_i, updates the bits of "real" */
+        i += 1
+      end
+      [n_live_box[0], live]
+      # p 7
     end
+
+    def update_sub(_ncodes, _n_live_box, _live)
+      false
+    end
+
+    def testmatch(_ring, _nchar, _live) = 0
   end
 end
