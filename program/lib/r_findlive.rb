78c7049c920ba4a8fba20c0de048a4a547c19e2c
righ1113 <34955220+righ1113@users.noreply.github.com>
2022-01-10 13:34:42 +0900

    22/01/10 reduce : Findlive のリファクタリング

--- program/lib/r_findlive.rb
+++ program/lib/r_findlive.rb
@@ -15,8 +15,6 @@ module Findlive
       ncodes  = (Const::POWER[ring] + 1) / 2     # number of codes of colorings of R
       bigno   = (Const::POWER[ring + 1] - 1) / 2 # needed in "inlive"
       @live   = Array.new(ncodes, 1)
-      # real0   = Array.new(Const::SIMATCHNUMBER[Const::MAXRING] / 8 + 2, 255)
-      # nchar   = Const::SIMATCHNUMBER[ring] / 8 + 1
 
       findlive ring, bigno, ncodes, angle, g_conf[0 + 1][2]
     end
@@ -28,10 +26,8 @@ module Findlive
       # colorings of the ring that are not restrictions of tri-colorings of the
       # free extension. Returns the number of such codes
       ed             = angle[0][2]
-      c              = Array.new(Const::EDGES, 0)
-      j              = ed - 1
-      c[ed]          = 1
-      c[j]           = 2
+      c, j           = Array.new(Const::EDGES, 0), ed - 1
+      c[ed], c[j]    = 1, 2
       forbidden      = Array.new(Const::EDGES, 0)
       forbidden[j]   = 5
       @n_live, @live = findlive_sub bigno, angle, ring, ed, extentclaim, ncodes, j, c, forbidden
@@ -43,57 +39,45 @@ module Findlive
         while (forbidden[jjj] & ccc[jjj]) != 0
           ccc[jjj] <<= 1
           while (ccc[jjj] & 8) != 0
-            if jjj >= edd - 1
-              print_status ring, ncodes, extent, extentclaim
-              return [ncodes - extent, @live]
-            end
+            (print_status ring, ncodes, extent, extentclaim; return [ncodes - extent, @live]) if jjj >= edd - 1
             jjj += 1
             ccc[jjj] <<= 1
           end
         end
+
         if jjj == ring + 1
           extent = record ccc, ring, angle, extent, bigno;
           ccc[jjj] <<= 1
           while (ccc[jjj] & 8) != 0
-            if jjj >= edd - 1
-              print_status ring, ncodes, extent, extentclaim
-              return [ncodes - extent, @live]
-            end
+            (print_status ring, ncodes, extent, extentclaim; return [ncodes - extent, @live]) if jjj >= edd - 1
             jjj += 1
             ccc[jjj] <<= 1
           end
         else
           jjj -= 1
           return [ncodes - extent, @live] if jjj.negative?
-          ccc[jjj] = 1
-          u = 0
-          angle[jjj][0].times do |ii|
-            i = ii + 1
-            break 0 if i >= 5
-            u |= ccc[angle[jjj][i]]
-          end
+          ccc[jjj], u = 1, 0
+          (1..angle[jjj][0]).each { |i| break (0..1) if i >= 5; u |= ccc[angle[jjj][i]] }
           forbidden[jjj] = u
         end
       end
-      # Assert.assert_equal (1 == 2), true, 'findlive_sub : It was not good though it was repeated 262144 times!'
+      Assert.assert_equal (1 == 2), true, 'findlive_sub : It was not good though it was repeated 262144 times!'
       [-1, @live]
     end
 
     def record(col, ring, angle, extent, bigno)
       # Given a colouring specified by a 1,2,4-valued function "col", it computes
       # the corresponding number, checks if it is in live, and if so removes it.
-
       weight = [0, 0, 0, 0, 0]
-      ring.times do |ii|
-        i = ii + 1
+      (1..ring).each do |i|
         sum = 7 - col[angle[i][1]] - col[angle[i][2]]
         sum = sum >= 5 ? 4 : sum
         sum = sum <= -1 ? 0 : sum
         weight[sum] += Const::POWER[i]
       end
+
       min = max = weight[4]
-      2.times do |ii|
-        i = ii + 1
+      (1..2).each do |i|
         w = weight[i]
         if w < min
           min = w
@@ -101,20 +85,17 @@ module Findlive
           max = w
         end
       end
-      colno = bigno - 2 * min - max
-      if live[colno] != 0
-        extent += 1
-        live[colno] = 0
-      end
 
+      colno = bigno - 2 * min - max
+      (extent += 1; @live[colno] = 0) if @live[colno] != 0
       extent
     end
 
-    def print_status(ring, totalcols, extent, _extentclaim)
+    def print_status(ring, totalcols, extent, extentclaim)
       print "\n\n   This has ring-size #{ring}, so there are #{totalcols} colourings total,\n"
       print "   and #{Const::SIMATCHNUMBER[ring]} balanced signed matchings.\n"
       print "\n   There are #{extent} colourings that extend to the configuration."
-      # Assert.assert_equal (extent == extentclaim), true, '***ERROR: DISCREPANCY IN NUMBER OF EXTENDING COLOURINGS***'
+      Assert.assert_equal (extent == extentclaim), true, '***ERROR: DISCREPANCY IN NUMBER OF EXTENDING COLOURINGS***'
       print "\n\n            remaining               remaining balanced\n"
       print "           colourings               signed matchings\n"
       printf "\n             %04d", (totalcols - extent)
